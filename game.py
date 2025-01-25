3c07738578d817ceb2afa4b298a5c19f2a9d44d1
xiaoyu <zhanglexuan100206@outlook.com>
2025-01-25 21:29:24 +0800

    判断五子连线

--- game.py
+++ game.py
@@ -4,6 +4,7 @@ WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
 
 player = 1
+winner = 0
 
 #二维列表
 map = [0] * 15
@@ -13,6 +14,69 @@ for i in range(15):
 pygame.init()
 SCREEN = pygame.display.set_mode((750, 750))
 pygame.display.set_caption("五子棋")
+
+#五子连珠
+def check(row, col):
+    #判断左右方向
+    score = 1
+    for i in range(4):
+        if map[row][col + i] == map[row][col + i + 1]:
+            score += 1
+        else:
+            break
+    for i in range(4):
+        if map[row][col - i] == map[row][col - i - 1]:
+            score += 1
+        else:
+            break
+    if score == 5:
+            return True
+
+    #判断上下方向
+    score = 1
+    for i in range(4):
+        if map[row + i][col] == map[row + i + 1][col]:
+            score += 1
+        else:
+            break
+    for i in range(4):
+        if map[row - i][col] == map[row - i - 1][col]:
+            score += 1
+        else:
+            break
+    if score == 5:
+            return True
+
+    #判断左斜方向
+    score = 1
+    for i in range(4):
+        if map[row + i][col + i] == map[row + i + 1][col + i + 1]:
+            score += 1
+        else:
+            break
+    for i in range(4):
+        if map[row - i][col - i] == map[row - i - 1][col - i - 1]:
+            score += 1
+        else:
+            break
+    if score == 5:
+            return True
+
+    #判断右斜方向
+    score = 1
+    for i in range(4):
+        if map[row + i][col - i] == map[row + i + 1][col - i - 1]:
+            score += 1
+        else:
+            break
+    for i in range(4):
+        if map[row - i][col + i] == map[row - i - 1][col + i + 1]:
+            score += 1
+        else:
+            break
+    if score == 5:
+            return True
+
 
 run = True
 while run:
@@ -25,6 +89,8 @@ while run:
             col = round((x - 25)/50)
             if map[row][col] == 0:
                 map[row][col] = player
+                if(check(row, col)):
+                    winner = player
                 if player == 1:
                     player = 2
                 else:
@@ -58,6 +124,18 @@ while run:
                 pygame.draw.circle(SCREEN, BLACK, (col*50+25, row*50+25), 25)
             elif map[row][col] == 2:
                 pygame.draw.circle(SCREEN, WHITE, (col*50+25, row*50+25), 25)
+
+    if winner != 0:
+        if winner == 1:
+            text = "黑子赢了"
+            color = BLACK
+        if winner == 2:
+            text = "白子赢了"
+            color = WHITE
+        font = pygame.font.Font("font.ttf", 70)
+        font_surface = font.render(text, True, color)
+        font_pos = (100, 100)
+        SCREEN.blit(font_surface, font_pos)
 
     pygame.display.update()
 
